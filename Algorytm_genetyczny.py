9d5dda06fa08e5ed16481fb8d51250f139ecc3e6
Jakub Wasiczek <qbawasiczek@gmail.com>
2025-01-09 17:56:04 +0100

    Zmiany w al_genetycznym

--- Algorytm_genetyczny.py
+++ Algorytm_genetyczny.py
@@ -3,110 +3,85 @@ import pandas as pd
 import os
 import random
 
-wd = os.getcwd()
-dane = pd.read_excel(wd + '/dane/Dane_TSP_48.xlsx', index_col=0, header=0)
-dane = np.array(dane)
+wd = os.path.dirname(os.path.realpath(__file__))
+dane1 = pd.read_excel(wd+'/dane/Dane_TSP_48.xlsx', index_col= 0, header = 0)
+dane2 = pd.read_excel(wd+'/dane/Dane_TSP_76.xlsx', index_col= 0, header = 0)
+dane3 = pd.read_excel(wd+'/dane/Dane_TSP_127.xlsx', index_col= 0, header = 0)
+dane1 = np.array(dane1)
+dane2 = np.array(dane2)
+dane3 = np.array(dane3)
 
-#Funkcja obliczająca długość trasy
-def path_length(route, dist_matrix):
-    length = sum(dist_matrix[route[i], route[i + 1]] for i in range(len(route) - 1))
-    length += dist_matrix[route[-1], route[0]]  # Powrót do miasta startowego
+np.fill_diagonal(dane2,0) #Plik _76 nie zawierał 0 po przekątnych
+
+def path_length(path, dist_matrix):
+    length = sum(dist_matrix[path[i], path[i + 1]] for i in range(len(path) - 1))
+    length += dist_matrix[path[-1], path[0]]  #Powrót do miasta startowego
     return length
 
-#Algorytm najbliższego sąsiada - inicjalizacja populacji
-def nn(dane):
-    population = {}
+def nn(dane): #Posłuży nam do wygenerowania początkowej populacji
 
-    for start in range(len(dane)):  
+    population = []  #Przechowywanie wszystkich wygenerowanych tras
+
+    for start in range(len(dane)):  #Testujemy różne startowe miasta
         visited_cities = [start]
 
         while len(visited_cities) < len(dane):
-            mask = dane[visited_cities[-1]].copy()
+            mask = dane[visited_cities[-1]].copy() 
             for i in visited_cities:
                 mask[i] = max(mask)  #Zapobiegam wybieraniu odwiedzonych miast
-
             next_city = np.argmin(mask)
-            visited_cities.append(next_city)
+            visited_cities.append(int(next_city))
 
-        visited_cities.append(start)  #Powrót do startu
-        for i in range(len(visited_cities)):
-            visited_cities[i] = int(visited_cities[i])
-        road_length = path_length(visited_cities, dane)
-        population[tuple(visited_cities)] = int(road_length)
+        population.append(visited_cities) 
 
-    return population
+    return population #Zwracamy listę list z indeksami ścieżek 
 
-#Funkcja selekcji turniejowej (wybiera najlepsze rozwiązania)
-def tournament_selection(population, k=3):
-    candidates = random.sample(list(population.keys()), k)
-    best = min(candidates, key=lambda route: population[route])
-    return best
+## DODAJ Co najmniej dwie metody selekcji i dwie metody krzyżowania ##
 
-#Krzyżowanie OX (Ordered Crossover)
-def ordered_crossover(parent1, parent2):
-    size = len(parent1) - 1  #Ostatni element to powrót do startu
-    start, end = sorted(random.sample(range(1, size), 2))  #Nie zmieniamy miasta startowego
+def roulette(init_pop, dist_matrix):
+    tot_path = 0
+    probs = []  # Prawdopodobieństwo bycia wybranym
+    
+    # Obliczanie całkowitej długości ścieżek
+    for i in range(len(init_pop)):
+        tot_path += path_length(init_pop[i], dist_matrix)
 
-    child = [-1] * (size + 1)
-    child[start:end] = parent1[start:end]
+    # Obliczanie prawdopodobieństw
+    for i in range(len(init_pop)):
+        probs.append(1 - (path_length(init_pop[i], dist_matrix) / tot_path))  # Dodajemy prawdopodobieństwo
+    
+    # Normalizacja prawdopodobieństw
+    total_prob = sum(probs)
+    normalized_probs = [p / total_prob for p in probs]
+    
+    # Skumulowane prawdopodobieństwa
+    cumulative_probs = [sum(normalized_probs[:i+1]) for i in range(len(normalized_probs))]
+    
+    selected = []
+    for j in range(int(len(init_pop) * 3 / 5)):
+        r = random.random()  # Losujemy liczbę z przedziału [0, 1)
+        
+        # Wybieramy element na podstawie skumulowanego prawdopodobieństwa
+        for i, cumulative_prob in enumerate(cumulative_probs):
+            if r <= cumulative_prob:
+                selected.append(init_pop[i])  # Dodajemy do listy wybranych
+                break
+    
+    return selected
 
-    #Wypełnianie brakujących miast z parent2
-    idx = end
-    for gene in parent2[1:size]:  #Pomijamy miasto startowe
-        if gene not in child:
-            if idx >= size:
-                idx = 1  #Pomijamy startowe miasto
-            child[idx] = gene
-            idx += 1
+def genetic_algorithm(dist_matrix, max_gen=100, mutation_prob=0.01):
+    init_pop = nn(dist_matrix)  # Inicjalizacja populacji (zakładając, że nn() jest zdefiniowane)
 
-    child[-1] = child[0]  #Powrót do miasta startowego
-    return tuple(child)
+    # Selekcja
+    selected = roulette(init_pop, dist_matrix)
 
-#Mutacja swap (zamienia miejscami dwa losowe miasta)
-def swap_mutation(route):
-    route = list(route)
-    size = len(route) - 1
-    a, b = random.sample(range(1, size), 2)  #Nie zmieniamy miasta startowego
-    route[a], route[b] = route[b], route[a]
-    return tuple(route)
+    print(selected)
+    #Metoda turnieju 
+    #Potencjalnie elitist
 
-#Algorytm genetyczny
-def genetic_algorithm(dane, max_gen=100, pop_size=30, crossover_rate=0.8, mutation_rate=0.2):
-    #Inicjalizacja populacji
-    population = nn(dane)
+    #Krzyżowanie
 
-    for gen in range(1, max_gen + 1):
-        new_population = {}
+    #Mutacja
 
-        while len(new_population) < pop_size:
-            #Selekcja rodziców
-            parent1 = tournament_selection(population)
-            parent2 = tournament_selection(population)
 
-            #Krzyżowanie
-            if random.random() < crossover_rate:
-                child = ordered_crossover(parent1, parent2)
-            else:
-                child = parent1  #Bez zmian
-
-            #Mutacja
-            if random.random() < mutation_rate:
-                child = swap_mutation(child)
-
-            #Obliczenie długości trasy dziecka
-            road_length = path_length(child, dane)
-            new_population[child] = road_length
-
-        #Aktualizacja populacji
-        population = new_population
-
-        #Wyświetlenie najlepszej trasy w generacji
-        best_route = min(population, key=population.get)
-        print(f"Generacja {gen}: Najlepsza długość trasy = {population[best_route]}")
-
-    return min(population, key=population.get), population[min(population, key=population.get)]
-
-best_route, best_length = genetic_algorithm(dane, max_gen=100, pop_size=30)
-print("\nNajlepsza trasa:", best_route)
-print("Najkrótsza długość trasy:", best_length)
 
